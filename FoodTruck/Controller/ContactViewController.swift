312f49d8449f5d0e65bcc978b3243c523fe9393a
Jada White <jadaw.2021@gmail.com>
2021-06-29 22:50:49 -0400

    Added Submit Events View and View Controller, and Contact View and View Controller.

---
+++ FoodTruck/Controller/ContactViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ContactViewController.swift
+//  FoodTruck
+//
+//  Created by Jada White on 6/29/21.
+//
+
+import UIKit
+
+class ContactViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
---
+++ FoodTruck/Controller/SubmitEventsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SubmitEventsViewController.swift
+//  FoodTruck
+//
+//  Created by Jada White on 6/29/21.
+//
+
+import UIKit
+
+class SubmitEventsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
