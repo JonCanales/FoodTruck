5cd6f3d6489b9de1c097095ca5477862c328144f
Jada White <jadaw.2021@gmail.com>
2021-06-30 08:24:19 -0400

    Started contact buttons functionality for Contact View Controller.

--- FoodTruck/Controller/ContactViewController.swift
+++ FoodTruck/Controller/ContactViewController.swift
@@ -3,7 +3,7 @@
 //  FoodTruck
 //
 //  Created by Jada White on 6/29/21.
-//
+// "On my honor, I have neither received nor given any unauthorized assistance on this assignment.” -- Jada White
 
 import UIKit
 
@@ -15,4 +15,15 @@ class ContactViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func websiteButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func numberButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func emailButtonPressed(_ sender: UIButton) {
+        
+    }
 }
