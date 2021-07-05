9cc3840e9594425c982df97db24f69e6818d3142
Jada White <jadaw.2021@gmail.com>
2021-07-05 19:33:52 -0400

    Added Google Calendar web view.

--- FoodTruck/Controller/SubmitEventsViewController.swift
+++ FoodTruck/Controller/SubmitEventsViewController.swift
@@ -3,76 +3,27 @@
 //  FoodTruck
 //
 //  Created by Jada White on 6/29/21.
-//
+// "On my honor, I have neither received nor given any unauthorized assistance on this assignment.” -- Jada White
 
 import UIKit
-import EventKitUI
+import WebKit
 
-class SubmitEventsViewController: UIViewController, EKEventViewDelegate, EKCalendarChooserDelegate {
+class SubmitEventsViewController: UIViewController, WKNavigationDelegate {
+    @IBOutlet weak var webView: WKWebView!
     
-    // Stores system calendar access request
-    let store = EKEventStore()
-    
-    private func requestCalendarAccess(completion: @escaping EKEventStoreRequestAccessCompletionHandler) {
-        // Request access to reminders.
-        store.requestAccess(to: .reminder) { granted, error in
-            // Handle the response to the request.
-        }
+    override func loadView() {
+        // Makes web view fullscreen
+        webView = WKWebView()
+        // Locks web view only to Google Calendar New Event input page
+        webView.navigationDelegate = self
+        view = webView
+        
     }
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Add event button
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(didTapAdd))
-//        self.dismiss(animated: true, completion: nil)
-
-    }
-    
-    @objc func didTapAdd() {
-        // Allows user to choose calendars stored on their phone
-//        let viewCtrl = EKCalendarChooser()
-//        viewCtrl.showsDoneButton = true
-//        viewCtrl.showsCancelButton = true
-//        // Make done and cancel buttons to work
-//        viewCtrl.delegate = self
-//        present(UINavigationController(rootViewController: viewCtrl), animated: true)
-        
-        // Request user access to calendar data
-        // Weak self -- prevents memory leakage
-        store.requestAccess(to: .event) { [weak self] success, error in
-            if success, error == nil {
-                DispatchQueue.main.async {
-                    guard let store = self?.store else { return }
-                    
-                    // New event will get stored on the device
-                    let newEvent = EKEvent(eventStore: store)
-                    
-                    // Event properties
-                    newEvent.title = ""
-                    newEvent.startDate = Date()
-                    newEvent.endDate = Date()
-                    newEvent.location = ""
-                    
-                    let viewCtrl = EKEventEditViewController()
-                    viewCtrl.eventStore = store
-                    // New event -- no info to show upon opening
-                    viewCtrl.event = newEvent
-                    self?.present(viewCtrl, animated: true, completion: nil)
-                    
-                    do {
-                      try store.save(newEvent, span: .thisEvent)
-                    } catch {
-                        print("Error when saving event: \(error.localizedDescription)")
-                    }
-                }
-            }
+        if let gCal = URL(string: "https://calendar.google.com/calendar/embed?src=c_vbbh2u006lr1n8i5t26tpt05n4%40group.calendar.google.com&ctz=America%2FNew_York") {
+            webView.load(URLRequest(url: gCal))
         }
-    }
-    
-    func eventViewController(_ controller: EKEventViewController, didCompleteWith action: EKEventViewAction) {
-        // Dismisses modal view of adding event
-        controller.dismiss(animated: true, completion: nil)
     }
     
 }
